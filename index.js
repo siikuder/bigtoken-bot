f0d21725e4ddbcbeb35b705bf4cd0d2d156808b1
wfajriansyahh <wfajriansyahh@gmail.com>
2019-04-10 16:40:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ async function registerBigToken(email, referral) {
             }
         } else {
             console.log(`[?] Failed to register. ${doGetData.email}@${emailVerified}\n`);
+            await delay(3000);
         }
     }
 })();
